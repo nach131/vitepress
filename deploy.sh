cdda6601108882c7a28eeb592e570ba204dff535
Nacho Mota 🚀 <nacho.mota@gmail.com>
2023-12-02 20:07:11 +0100

    inicio

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+set -e
+
+npm run build
+
+cd dist
+
+git init
+git checkout -b main
+git add -A
+git commmit -m 'deploy'
+git push -f git@github.com:nach131/demo-web.git main:nach-pages
